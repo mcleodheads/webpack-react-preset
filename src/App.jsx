c52c7983cf6a679466d16f6371603e3e5c49e0a5
mcleodheads <vamaklakov@mmtr.ru>
2021-10-26 18:02:59 +0300

    Preset

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './assets/styles/style.scss'
+
+const App = () => {
+    return (
+        <div className={'wrapper'}>
+            <div className={'word'}>
+                yoda
+            </div>
+            <div className={'image'}></div>
+        </div>
+
+    );
+};
+
+export default App;
